6096a68cf14fa33455f40ac0c93f5306be089df5
markfan <fanzhengchen@gmail.com>
2018-06-22 20:50:47 +0800

    hive sychronized to hbase

---
+++ hive-client/src/main/java/com/xgn/hiveclient/HiveConfiguration.java
@@ -0,0 +1,13 @@
+package com.xgn.hiveclient;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * Date: 2018-06-22
+ * Time: 5:49 PM
+ *
+ * @author: MarkFan
+ * @since v1.0.0
+ */
+public class HiveConfiguration {
+}
---
+++ hive-client/src/main/java/com/xgn/hiveclient/service/HiveService.java
@@ -0,0 +1,13 @@
+package com.xgn.hiveclient.service;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * Date: 2018-06-22
+ * Time: 3:09 PM
+ *
+ * @author: MarkFan
+ * @since v1.0.0
+ */
+public class HiveService {
+}
